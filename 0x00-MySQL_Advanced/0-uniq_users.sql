a2aedd25b37c8bc73b8bd1afe8275b8c82fc34d7
Evance Odoyo <odoyoevanc@gmail.com>
2024-05-12 13:58:36 +0300

    remove trailing comma in statement

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
 -- create a user table with id, email, name, and country columns
-CREATE TABLE IF NOT EXISTS users (
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) UNIQUE NOT NULL,
-	name VARCHAR(255),
+	name VARCHAR(255)
 );
