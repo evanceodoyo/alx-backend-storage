e37263c9a016c8440b0f9ab96459e8bc719255f9
Evance Odoyo <odoyoevanc@gmail.com>
2024-05-12 03:33:33 +0300

    column with enum type

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,6 @@
 -- create a user table with id, email, name, and country columns
-CREATE TABLE IF NOT EXISTS users (
+DROP TABLE IF EXISTS user;
+CREATE TABLE users (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) UNIQUE NOT NULL,
 	name VARCHAR(255),
