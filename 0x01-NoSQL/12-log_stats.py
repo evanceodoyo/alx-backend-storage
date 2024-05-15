85a6d5efc5fb0a37a63ec65ef2e6dc9eb1f88e98
Evance Odoyo <odoyoevanc@gmail.com>
2024-05-15 21:21:09 +0300

    change the count approach

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,30 +5,28 @@ Module to prints Nginx logs in a MongoDB
 from pymongo import MongoClient
 
 
-def nginx_stats(mongo_collection):
+def print_nginx_stats(mongo_collection):
     """
     Function to provide stats about Nginx logs stored in MongoDB.
     """
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     num_logs = mongo_collection.count_documents({})
-    num_status_check = mongo_collection.count_documents(
+    num_status_check = len(list(mongo_collection.find(
                 {"method": "GET", "path": "/status"}
-            )
+            )))
 
     print("{} logs".format(num_logs))
     print("Methods:")
     for method in methods:
-        print("\tmethod {}: {}".format(
-            method,
-            mongo_collection.count_documents({"method": method}))
-        )
+        count = len(list(mongo_collection.find({"method": method})))
+        print("\tmethod {}: {}".format(method, count))
     print("{} status check".format(num_status_check))
 
 
 def run():
     client = MongoClient("mongodb://127.0.0.1:27017")
     nginx_collection = client.logs.nginx
-    nginx_stats(nginx_collection)
+    print_nginx_stats(nginx_collection)
 
 
 if __name__ == "__main__":
