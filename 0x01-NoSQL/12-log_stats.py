f45d25689a54b289f028424a52b5614439f1dd68
Evance Odoyo <odoyoevanc@gmail.com>
2024-05-15 21:26:31 +0300

    refactor the code

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -23,12 +23,6 @@ def print_nginx_stats(mongo_collection):
     print("{} status check".format(num_status_check))
 
 
-def run():
-    client = MongoClient("mongodb://127.0.0.1:27017")
-    nginx_collection = client.logs.nginx
-    print_nginx_stats(nginx_collection)
-
-
 if __name__ == "__main__":
-    """Driver function"""
-    run()
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    print_nginx_stats(client.logs.nginx)
