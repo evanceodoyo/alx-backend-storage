2fa5d4212eb10d4335c4ffc10d179337fde60305
Evance Odoyo <odoyoevanc@gmail.com>
2024-05-15 22:17:19 +0300

    aggregation - finding average: python

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""aggregation"""
+
+
+def top_students(mongo_collection):
+    """Function that returns all students sorted by average score."""
+    return mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
