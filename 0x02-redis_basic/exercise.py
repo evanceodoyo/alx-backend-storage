e71f771aff27190522a5829ad26a4ea59f072d66
Evance Odoyo <odoyoevanc@gmail.com>
2024-05-16 03:44:06 +0300

    decorator for incrementing values everytime a method is called

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,21 @@ Writing strings to redis.
 """
 import redis
 import uuid
+from functoools import wraps
 from typing import Union, Callable
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    Decorator function that returns a Callable.
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper function"""
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -18,6 +32,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Stores data in Redis using a randomly generated uuid as key.
